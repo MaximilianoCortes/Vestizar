72ef25a7bf4df244406a4f64464d7694cc3a7a99
MaximilianoCortes <89611132+MaximilianoCortes@users.noreply.github.com>
2022-09-28 15:04:35 -0300

    Initial commit

---
+++ src/main/java/com/example/vestizar/VestizarApplication.java
@@ -0,0 +1,10 @@
+package com.example.vestizar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestizarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VestizarApplication.class, args
