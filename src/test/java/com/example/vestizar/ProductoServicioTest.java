8fcee1cdc28a481d347ddd66b8197f9ac4b47cb0
tobaruu <cristobalamp@gmail.com>
2022-11-19 19:34:47 -0300

    primeros 2 tests funcionales

--- src/test/java/com/example/vestizar/ProductoServicioTest.java
+++ src/test/java/com/example/vestizar/ProductoServicioTest.java
@@ -27,13 +27,16 @@ public class ProductoServicioTest {
     @Autowired
     private ProductoServicio productoServicio;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         producto1 = new Producto(0,"Polera","Hombre","Semi-Nuevo","M","Camisa con poco uso negra marca Maui","Maui",5000);
+        productoServicio.guardarProducto(producto1);
         IdProducto1 = producto1.getIdPoroducto();
         producto2 = new Producto(0,"Pantalon","Mujer","Usado","S","Pantalon Levi's usado","Levi's",10000);
+        productoServicio.guardarProducto(producto2);
         IdProducto2 = producto2.getIdPoroducto();
         producto3 = new Producto(1,"Poleron","Niños","Nuevo","XL","Poleron gris nuevo para niños marca Nomade","Nomade",7000);
+        productoServicio.guardarProducto(producto3);
         IdProducto3 = producto3.getIdPoroducto();
         productos = Arrays.asList(producto1, producto2, producto3);
     }
@@ -46,33 +49,37 @@ public class ProductoServicioTest {
         producto3 = null;
     }
 
+
+    @AfterEach
+    void teardown() {
+        productoServicio.eliminarProducto(IdProducto1);
+        productoServicio.eliminarProducto(IdProducto2);
+        productoServicio.eliminarProducto(IdProducto3);
+    }
+
     @DisplayName("Obtener producto por ID")
     @Test
     public void obtenerProductoPorId(){
-        productoServicio.guardarProducto(producto1);
-        productoServicio.guardarProducto(producto2);
-        productoServicio.guardarProducto(producto3);
 
         for (Producto producto : productos) {
-            if(producto != null){
-                Assertions.assertNotNull(producto);
-                System.out.println("Producto "+producto.getIdPoroducto()+" encontrado");
-                Assertions.assertNull(producto.getImagen1());
-                Assertions.assertNotEquals(0, (long) producto.getIdPoroducto());
-                if(producto.getIdPoroducto() == IdProducto1) {
-                    Assertions.assertEquals(IdProducto1, producto.getIdPoroducto());
-                }else if(producto.getIdPoroducto() == IdProducto2) {
-                    Assertions.assertEquals(IdProducto2, producto.getIdPoroducto());
+
+            Producto productoEncontrado = productoServicio.obtenerProductoPorId(producto.getIdPoroducto());
+            if(productoEncontrado != null){
+                Assertions.assertNotNull(productoEncontrado);
+                System.out.println("Producto "+productoEncontrado.getIdPoroducto()+" encontrado");
+                Assertions.assertNull(productoEncontrado.getImagen1());
+                Assertions.assertNotEquals(0, (long) productoEncontrado.getIdPoroducto());
+                if(productoEncontrado.getIdPoroducto() == IdProducto1) {
+                    Assertions.assertEquals(IdProducto1, productoEncontrado.getIdPoroducto());
+                }else if(productoEncontrado.getIdPoroducto() == IdProducto2) {
+                    Assertions.assertEquals(IdProducto2, productoEncontrado.getIdPoroducto());
                 }
-                else if(producto.getIdPoroducto() == IdProducto3) {
-                    Assertions.assertEquals(IdProducto3, producto.getIdPoroducto());
+                else if(productoEncontrado.getIdPoroducto() == IdProducto3) {
+                    Assertions.assertEquals(IdProducto3, productoEncontrado.getIdPoroducto());
                 }
             }
         }
 
-        productoServicio.eliminarProducto(producto1.getIdPoroducto());
-        productoServicio.eliminarProducto(producto2.getIdPoroducto());
-        productoServicio.eliminarProducto(producto3.getIdPoroducto());
 
 
     }
@@ -81,28 +88,34 @@ public class ProductoServicioTest {
     @Test
     public void guardarProducto(){
 
-            productoServicio.guardarProducto(producto1);
-
-            Producto productoEncontrado = productoServicio.obtenerProductoPorId(producto1.getIdPoroducto());
+        for (Producto producto : productos) {
+            Producto productoEncontrado = productoServicio.obtenerProductoPorId(producto.getIdPoroducto());
 
             if(productoEncontrado != null){
                 Assertions.assertNotNull(productoEncontrado);
                 Assertions.assertNotEquals(0, (long) productoEncontrado.getIdPoroducto());
                 Assertions.assertFalse(productoEncontrado.getIdPoroducto() <= 0);
-                System.out.println("Producto "+productoEncontrado.getIdPoroducto()+" encontrado a partir de la busqueda del producto "+producto1.getIdPoroducto());
-                Assertions.assertEquals(producto1.getIdPoroducto(), productoEncontrado.getIdPoroducto());
-                Assertions.assertEquals(producto1.getAprobado(), productoEncontrado.getAprobado());
-                Assertions.assertEquals(producto1.getTipoProducto(), productoEncontrado.getTipoProducto());
-                Assertions.assertEquals(producto1.getCategoria(), productoEncontrado.getCategoria());
-                Assertions.assertEquals(producto1.getEstado(), productoEncontrado.getEstado());
-                Assertions.assertEquals(producto1.getTalla(), productoEncontrado.getTalla());
-                Assertions.assertEquals(producto1.getDescripcion(), productoEncontrado.getDescripcion());
-                Assertions.assertEquals(producto1.getMarca(), productoEncontrado.getMarca());
-                Assertions.assertEquals(producto1.getPrecio(), productoEncontrado.getPrecio());
+                System.out.println("Producto "+productoEncontrado.getIdPoroducto()+" encontrado a partir de la busqueda del producto "+producto.getIdPoroducto());
+                Assertions.assertEquals(producto.getIdPoroducto(), productoEncontrado.getIdPoroducto());
+                Assertions.assertEquals(producto.getAprobado(), productoEncontrado.getAprobado());
+                Assertions.assertEquals(producto.getTipoProducto(), productoEncontrado.getTipoProducto());
+                Assertions.assertEquals(producto.getCategoria(), productoEncontrado.getCategoria());
+                Assertions.assertEquals(producto.getEstado(), productoEncontrado.getEstado());
+                Assertions.assertEquals(producto.getTalla(), productoEncontrado.getTalla());
+                Assertions.assertEquals(producto.getDescripcion(), productoEncontrado.getDescripcion());
+                Assertions.assertEquals(producto.getMarca(), productoEncontrado.getMarca());
+                Assertions.assertEquals(producto.getPrecio(), productoEncontrado.getPrecio());
 
             }
+        }
 
-            productoServicio.eliminarProducto(producto1.getIdPoroducto());
+
+    }
+
+    @Disabled
+    @DisplayName("Obtener productos aprobados")
+    @Test
+    public void obtenerProductoPorAprobado(){
 
     }
 
